d9f37c616a7b4eb17c3b31c28fe79b18251813e6
Simran Shaikh <simmoshaikh20.2003@gmail.com>
2025-01-26 18:15:23 +0530

    commit

---
+++ PartC/vector_manipulation.R
@@ -0,0 +1,11 @@
+# Manipulation on Vector data structure
+data <- 1:20
+print(data)
+new_data<-data+3
+print(paste("New Data after added 3 :",new_data))
+print(paste("5th element:", data[5]))
+print(paste("10th element:", data[10]))
+print(paste("Last element:", data[length(data)]))
+print(paste("Sum of vector:", sum(data)))
+print(paste("Mean of vector:", mean(data)))
+print(paste("Length of vector:", length(data)))
