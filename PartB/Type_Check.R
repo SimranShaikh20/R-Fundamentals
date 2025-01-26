56c241877f44cf93d49878c0e57f003b574b53aa
Simran Shaikh <simmoshaikh20.2003@gmail.com>
2025-01-26 17:52:38 +0530

    commit

---
+++ PartB/Type_Check.R
@@ -0,0 +1,14 @@
+#Check Type of Data
+num=10
+str="Simran Shaikh"
+flag= TRUE
+print("--Using Class method-----")
+print(class(num))
+print(class(str))
+print(class(flag))
+print("-------------------")
+print("--Using typeof method-----")
+print(typeof(num))
+print(typeof(str))
+print(typeof(flag))
+print("-------------------")
---
+++ PartB/math_func.R
@@ -0,0 +1,5 @@
+# Math function
+print(paste("Absolute value of -15:", abs(-15)))
+print(paste("Round 3.567 to 2 decimal places:", round(3.567, 2)))
+print(paste("Ceiling of 4.2:", ceiling(4.2)))
+print(paste("Floor of 4.8:", floor(4.8)))
